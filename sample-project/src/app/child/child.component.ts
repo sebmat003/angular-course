a6aba7353f6147c43250a47068cf94551da3b981
sebmat003 <sebmat003@utp.edu.pl>
2024-03-13 22:26:16 +0100

    added material

--- sample-project/src/app/child/child.component.ts
+++ sample-project/src/app/child/child.component.ts
@@ -1,11 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   standalone: true,
   imports: [],
   templateUrl: './child.component.html',
-  styleUrl: './child.component.scss'
+  styleUrl: './child.component.scss',
 })
 export class ChildComponent {
   @Input() text: string;
